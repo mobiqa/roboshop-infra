03c3e9b817526e0a38728714f2bcbc90b72c2f15
venkat <venkat.mobiqa@gmail.com>
2023-02-12 16:37:01 +0530

    updating ./roboshop-infra

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
-resource "null_resource" "test" {
-  provisioner "local-exec" {
-    command = "echo ${var.env}"
-  }
+module "network" {
+  source = "github.com/mobiqa/tf-module-vpc"
+#  env    = var.env
+#
+   for_each   = var.vpc
+   cidr_block = each.value.cidr_block
 }
