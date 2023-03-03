9793aa96c9c889bb11124fa045e18ef4ff247f26
venkat <venkat.mobiqa@gmail.com>
2023-03-03 08:25:23 +0530

    updating ./roboshop-infra

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,9 +1,9 @@
 env            = "dev"
-default_vpc_id = "vpc-08048465f77f42b75"
+#default_vpc_id = "vpc-08048465f77f42b75"
 
 vpc = {
   main = {
     cidr_block   = "10.0.0.0/16"
-    subnets_cidr = ["10.0.0.0/17", "10.0.128.0/17"]
+#    subnets_cidr = ["10.0.0.0/17", "10.0.128.0/17"]
   }
 }
--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "network" {
   source         = "github.com/mobiqa/tf-module-vpc"
-  env            = var.env
-  default_vpc_id = var.default_vpc_id
+#  env            = var.env
+#  default_vpc_id = var.default_vpc_id
 
   for_each     = var.vpc
   cidr_block   = each.value.cidr_block
-  subnets_cidr = each.value.subnets_cidr
+#  subnets_cidr = each.value.subnets_cidr
 }
 
 
