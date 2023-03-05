64024b6994bc2723022ffb0523567e9718c6579d
venkat <venkat.mobiqa@gmail.com>
2023-03-05 12:37:35 +0530

    updating ./roboshop-infra

--- main.tf
+++ main.tf
@@ -12,11 +12,12 @@ module "subnets" {
   source         = "github.com/mobiqa/tf-module-subnets"
   env            = var.env
   default_vpc_id = var.default_vpc_id
-  vpc_id = module.vpc.vpc_id
 
   for_each   = var.subnets
   cidr_block = each.value.cidr_block
   availability_zone = each.value.availability_zone
   name = each.value.name
+  vpc_id = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+
 }
 
