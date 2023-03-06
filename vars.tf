f12c7aba205eb25bd544b844de2f5c7323333e66
venkat <venkat.mobiqa@gmail.com>
2023-03-06 14:25:43 +0530

    updating ./roboshop-infra

--- vars.tf
+++ vars.tf
@@ -1,7 +1,4 @@
 variable "env" {}
 variable "vpc" {}
 variable "default_vpc_id" {}
-variable "subnets" {}
-variable "public_subnets" {}
-variable "private_subnets" {}
-variable "availability_zone" {}
+
