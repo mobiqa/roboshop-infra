4d82b702a7d53d3f38c86919dff8103194a45bcc
venkat <venkat.mobiqa@gmail.com>
2023-03-12 15:49:56 +0530

    updating ./roboshop-infra

--- main.tf
+++ main.tf
@@ -39,6 +39,18 @@ module "rds" {
   instance_class      = each.value.instance_class
 }
 
+module "elasticache" {
+  source = "github.com/mobiqa/tf-module-elasticache"
+  env    = var.env
+
+  for_each        = var.elasticache
+  subnet_ids      = lookup(lookup(lookup(lookup(module.vpc, each.value.vpc_name, null), "private_subnet_ids", null), each.value.subnets_name, null), "subnet_ids", null)
+  vpc_id          = lookup(lookup(module.vpc, each.value.vpc_name, null), "vpc_id", null)
+  allow_cidr      = lookup(lookup(lookup(lookup(var.vpc, each.value.vpc_name, null), "private_subnets", null), "app", null), "cidr_block", null)
+  num_cache_nodes = each.value.num_cache_nodes
+  node_type       = each.value.node_type
+  engine_version  = each.value.engine_version
+}
 module "rabbitmq" {
   source = "github.com/mobiqa/tf-module-rabbitmq"
   env    = var.env
